887deb4262714364e1009be6d4a712c906e3eae8
zq <573078966@qq.com>
2019-10-17 16:02:07 +0800

    1.0

--- run_suite.py
+++ run_suite.py
@@ -7,7 +7,8 @@ from case.test_TPShop import TestTPShop
 
 suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(TestTPShop))
-file_to = "./report/{}_report.html".format(time.strftime("%Y%m%d%H%M%S"))
+file_to = "./report/tws_report.html"
+# file_to = "./report/tws_report.html".format(time.strftime("%Y%m%d%H%M%S"))
 with open(file_to,"wb") as f:
     runner = HTMLTestRunner(f,title="张倩的",description="v1.0")
     runner.run(suite)
